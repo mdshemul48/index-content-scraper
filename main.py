581207cbf98d360a39e2325c88e2f5b9100e6512
MD. Shimul <mdshemul480@gmail.com>
2022-01-21 16:44:34 +0600

    creating an object of tv series

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-from lib2to3.pgen2 import driver
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-
-
 from bs4 import BeautifulSoup
-from scraper import fetch_content
 
 
 class Scraper:
@@ -20,43 +16,47 @@ class Scraper:
             service=Service(ChromeDriverManager().install()), options=options
         )
         self.driver.get(url)
+        sleep(2)
         self.main_page_content = self.driver.page_source
         self.domain = domain
+        self.folder_link = url
 
-    def fetch_folder_info(self) -> dict:
-        html_tree = BeautifulSoup(self.main_page_content, "html.parser")
+    def fetch_list(self, link: str, item_type: str) -> list:
+        self.driver.get(link)
+        sleep(2)
+        html_tree = BeautifulSoup(self.driver.page_source, "html.parser")
         main_content_area = html_tree.find("div", {"id": "content"})
-        folders_soup = main_content_area.find_all("li", {"class": "item folder"})
-
+        folders_soup = main_content_area.find_all("li", {"class": f"item {item_type}"})
+        items = []
         for item in folders_soup:
             folder = item.find("a", href=True)
             folder_title = folder.find("span", {"class": "label"})["title"]
             folder_link = folder["href"]
-            self.folders.append(
-                {"title": folder_title, "link": self.domain + folder_link}
-            )
+            items.append({"title": folder_title, "link": self.domain + folder_link})
+        return items
 
+    def fetch_folder_info(self) -> dict:
+        self.folders = self.fetch_list(self.folder_link, "folder")
+        return self.folders
+
+    def fetch_folder_item(self):
+        for folder in self.folders:
+            folder["items"] = self.fetch_list(folder["link"], "file")
         return self.folders
 
     def close(self):
         self.driver.close()
 
 
-# def
-# def main():
-#     url_link = "http://ftp4.circleftp.net/FILE/English%20%26%20Foreign%20TV%20Series/Friends%20%28TV%20Series%201994-2004%29"
-
-#     current_domain = "http://ftp4.circleftp.net"
-#     page_content = fetch_content(url_link)
-#     content_information = fetch_folder_info(page_content, current_domain)
-#     print(content_information)
-
-
-if __name__ == "__main__":
+def main():
     current_domain = "http://ftp4.circleftp.net"
     url_link = "http://ftp4.circleftp.net/FILE/English%20%26%20Foreign%20TV%20Series/Friends%20%28TV%20Series%201994-2004%29"
 
     scraper = Scraper(current_domain, url_link)
-    print(scraper.fetch_folder_info())
-
+    scraper.fetch_folder_info()
+    print(scraper.fetch_folder_item())
     scraper.close()
+
+
+if __name__ == "__main__":
+    main()
